6c63698155115f60270ed7b38ffbd5cadb04ef4a
leonardo-bravop <leo.bravoparedes@gmail.com>
2025-01-20 16:37:27 -0500

    Add Preact hydrated greeting component

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://astrobloglbp.netlify.app"
+  site: "https://astrobloglbp.netlify.app",
+  integrations: [preact()]
 });
